e25f14124cf51dedb608b849e9a8d393bd1d060f
Łukasz <lukaszhur0604@gmail.com>
2022-03-04 18:05:43 +0100

    dodana metoda init, teraz plik z modelem jest otwierany jednokrotnie

--- predictions.py
+++ predictions.py
@@ -1,13 +1,18 @@
 import pickle
 
 
-class Img_here:
+class ImgHere:
+
+    def __init__(self, img_1x784):
+        self.predictions = self.clssifiter.predict(img_1x784)
+
     lista = []
-    def predictions(self, img_1x784):
-        with open("classifiter_SVC_+.pkl", "rb") as f:
-            clssifiter = pickle.load(f)
-        predictions = clssifiter.predict(img_1x784)
-        self.lista.append(predictions[0])
+    classifiter = open("classifiter_SVC_+.pkl", "rb")
+    clssifiter = pickle.load(classifiter)
+
+    def predictions_append(self):
+        self.lista.append(self.predictions[0])
 
     def detected(self):
-        return self.lista
+        print(self.lista)
+        self.classifiter.close()
