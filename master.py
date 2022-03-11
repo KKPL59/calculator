839c7a6b7d9038035993ab132f860a87b185f098
Łukasz <lukaszhur0604@gmail.com>
2022-03-11 22:01:07 +0100

    dodałem obraz, na podstawie którego wykonywana jest predykcja

--- master.py
+++ master.py
@@ -1,17 +1,16 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Line, Color
+import cv2
+from contours_copy2 import Pieces
 
 
 
 class WorkPlace(FloatLayout):
 
 
-    def printer(self):
-        return "akhf"
-
     def on_touch_down(self, touch):
-        with self.canvas:
+        with self.ids.paint.canvas:
             Color(0, 0, 0)
             if self.ids.paint.collide_point(touch.x, touch.y):
                 touch.ud['line'] = Line(points=(touch.x, touch.y), width=7)
@@ -22,12 +21,26 @@ class WorkPlace(FloatLayout):
             touch.ud['line'].points += [touch.x, touch.y]
         return super().on_touch_move(touch)
 
+    licznik = 0
     def on_touch_up(self, touch):
+        self.licznik += 1
         if self.ids.paint.collide_point(touch.x, touch.y):
-            pass
-            # print(self.)
+            self.export_to_png("JD.png")
+
+            img = cv2.imread("JD.png")
+            img = img[300:600, 0:350]
+
+            self.pieces = Pieces(img)
+            self.pieces.cutting_out()
+
+            self.prediction = str(self.pieces.prediction)
+            self.ids.mathematical_operations.text = self.prediction
+
+            self.ids.paint.canvas.clear()
         return super().on_touch_move(touch)
 
+    # def printer(self):
+    #     return str(self.licznik)
 
 
 class CalculatorApp(App):
@@ -35,5 +48,9 @@ class CalculatorApp(App):
     def build(self):
         return WorkPlace()
 
+
 buttonapp = CalculatorApp()
 buttonapp.run()
+
+
+
