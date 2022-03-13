d471039265b95f42b25aef2fa2951d7ad4e5b831
Łukasz <lukaszhur0604@gmail.com>
2022-03-13 18:30:57 +0100

    teraz działanie wyświetla się za pomozą funkcji crics

--- mathematical_operations.py
+++ mathematical_operations.py
@@ -1,8 +1,31 @@
 
-
-
 class Priner:
-    pass
+
+
+    def circs(self, operation):
+        self.text = ""
+        for i in enumerate(operation):
+            if i[1] == "10":
+                operation[i[0]] = "+"
+
+        for i in operation:
+            if i != " ":
+                i = int(i)
+                i = str(i)
+                self.text += i
+
+        return self.text
+
+
+    def operations(self):
+        self.text = ""
+
+
+
 
 class MathematicalOperations(Priner):
     pass
+
+
+# printer = Priner(["2", "10", "2"])
+# print(printer.circs())
