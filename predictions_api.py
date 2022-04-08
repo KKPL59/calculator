2e062837457df526a6e2e53249598fab99240daa
KKPL59 <74962186+KKPL59@users.noreply.github.com>
2022-04-08 17:45:18 +0200

    Update predictions_api.py

--- predictions_api.py
+++ predictions_api.py
@@ -4,7 +4,7 @@ import json
 
 
 class ImgHere:
-    api = 'https://imgpred.herokuapp.com/test'
+
 
     def __init__(self, img_1x784):
         """Doknuje predykcji na podtawie otrzymanego zdjęcia."""
@@ -15,7 +15,9 @@ class ImgHere:
             self.im_bytes = f.read()
         self.im_b64 = base64.b64encode(self.im_bytes).decode("utf8")
 
-    def get_prediction(self):
+    def get_prediction_digit(self):
+        self.api = "https://imgpred.herokuapp.com/digit"
+
         self.headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
         self.payload = json.dumps({"image": self.im_b64, "other_key": "value"})
@@ -25,7 +27,15 @@ class ImgHere:
 
         return self.req.result
 
-# img = cv2.imread("1_YpijSnfbzpF2118eYUeNCg.png")
-#
-# imghere = ImgHere(img)
-# print(imghere.get_prediction()[1])
+    def get_prediction_photo(self):
+        self.api = "https://imgpred.herokuapp.com/photo"
+
+        self.headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+        self.payload = json.dumps({"image": self.im_b64, "other_key": "value"})
+        self.req = UrlRequest(url=self.api, req_body=self.payload, req_headers=self.headers)
+
+        self.req.wait()
+
+
+        return self.req.result
