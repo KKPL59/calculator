b22fbeb3defcae7586130b539eeb296aded951a0
KKPL59 <74962186+KKPL59@users.noreply.github.com>
2022-04-21 22:43:32 +0200

    Update predictions_api.py

--- predictions_api.py
+++ predictions_api.py
@@ -1,3 +1,4 @@
+from kivy.app import App
 from kivy.network.urlrequest import UrlRequest
 import base64
 import json
@@ -11,18 +12,18 @@ class ImgHere:
 
         self.predictions = img_1x784
 
-        with open("digit.png", "rb") as f:
+        with open(f"{App().user_data_dir}digit.png", "rb") as f:
             self.im_bytes = f.read()
         self.im_b64 = base64.b64encode(self.im_bytes).decode("utf8")
 
     def get_prediction_digit(self):
         self.api = "https://imgpred.herokuapp.com/digit"
+        # self.api = "http://127.0.0.1:5000/digit"
 
         self.headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
         self.payload = json.dumps({"image": self.im_b64, "other_key": "value"})
         self.req = UrlRequest(url=self.api, req_body=self.payload, req_headers=self.headers)
-
         self.req.wait()
 
         return self.req.result
@@ -34,8 +35,7 @@ class ImgHere:
 
         self.payload = json.dumps({"image": self.im_b64, "other_key": "value"})
         self.req = UrlRequest(url=self.api, req_body=self.payload, req_headers=self.headers)
-
         self.req.wait()
 
-
+        # print(self.req.result)
         return self.req.result
